ea6cd32ddc3e55158953cfa08c72a9fe27c82e9d
acho04 <myemailiswhatyousee@gmail.com>
2026-06-30 14:23:13 +0900

    Update StatWorker_PokemonStats.cs

--- 1.6/Source/PokeWorld/Stats/StatWorker_PokemonStats.cs
+++ 1.6/Source/PokeWorld/Stats/StatWorker_PokemonStats.cs
@@ -61,12 +61,12 @@ internal class StatWorker_PokemonStats : StatWorker
                 var level = comp.levelTracker.level;
                 if (stat.defName == "PW_HP")
                 {
-                    val = (int)((2 * val + IV + EV / 4) * level / 200) + level + 20;
+                    val = (int)((2 * val + IV + EV / 4) * level / 200) + level + 30;
                 }
                 else
                 {
                     var natureMultiplier = GetNatureMultiplier(comp, stat);
-                    val = (int)(((2 * val + IV + EV / 4) * level / 200 + 10) * natureMultiplier);
+                    val = (int)(((2 * val + IV + EV / 4) * level / 200 + 25) * natureMultiplier);
                 }
             }
         }
